ecdbfb26aafea882a7f90450e35d429dbbc9eeda
Gessielle <gessielle--barossi@hotmail.com>
2020-09-13 00:42:29 -0300

    Letra da música girassol

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Música
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Girassol");
+            Console.WriteLine("Whindersson Nunes e Priscilla Alcantara");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.WriteLine("Se a vida fosse fácil como a gente quer");
+            Console.WriteLine("Se o futuro a gente pudesse prever");
+            Console.WriteLine("Eu hoje estaria tomando um café");
+            Console.WriteLine("Sentado com os amigos em frente a tv");
+            Console.Write("\n");
+
+            Console.WriteLine("Eu olharia as aves como eu nunca olhei");
+            Console.WriteLine("Daria um abraço apertado em meus avós");
+            Console.WriteLine("Diria eu te amo a quem nunca pensei");
+            Console.WriteLine("Talvez, é o que o universo espera de nós");
+            Console.Write("\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Eu quero ser curado e ajudar curar também");
+            Console.WriteLine("Eu quero ser melhor do que eu nunca fui");
+            Console.WriteLine("Fazer o que eu posso pra me ajudar");
+            Console.WriteLine("Ser justo e paciente como era Jesus");
+            Console.Write("\n");
+
+            Console.WriteLine("Eu quero dar valor até ao calor do sol");
+            Console.WriteLine("Que eu esteja preparado pra quem me conduz");
+            Console.WriteLine("Que eu seja todo dia como um girassol");
+            Console.WriteLine("De costas pro escuro e de frente pra luz");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.WriteLine("Eu olharia as aves como eu nunca olhei");
+            Console.WriteLine("Daria um abraço apertado em meus avós");
+            Console.WriteLine("Diria eu te amo a quem nunca pensei");
+            Console.WriteLine("Talvez, é o que o universo espera de nós");
+            Console.Write("\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Eu quero ser curado e ajudar curar também");
+            Console.WriteLine("Eu quero ser melhor do que eu nunca fui");
+            Console.WriteLine("Fazer o que eu posso pra me ajudar");
+            Console.WriteLine("Ser justo e paciente como era Jesus");
+            Console.Write("\n");
+
+            Console.WriteLine("Eu quero dar valor até ao calor do sol");
+            Console.WriteLine("Que eu esteja preparado pra quem me conduz");
+            Console.WriteLine("Que eu seja todo dia como um Girassol");
+            Console.WriteLine("De costas pro escuro e de frente pra luz");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.WriteLine("Que eu seja todo dia como um Girassol");
+            Console.WriteLine("De costas pro escuro e de frente pra luz");
+            Console.WriteLine("E de frente pra luz, oh");
+            Console.WriteLine("E de frente pra luz");
+            Console.WriteLine("Se a vida fosse fácil como a gente quer");
+        }
+    }
+}
